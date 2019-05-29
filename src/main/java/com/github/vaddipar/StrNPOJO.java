d89a99e0cc1ef6f144122024bf481c42375fdb1a
Sandeep Srivastav Vaddiparthy <vsandeepsrivastav@gmail.com>
2019-05-29 15:10:55 +0530

    moved to switched case due to speed

--- src/main/java/com/github/vaddipar/StrNPOJO.java
+++ src/main/java/com/github/vaddipar/StrNPOJO.java
@@ -16,6 +16,7 @@ public class StrNPOJO {
   public Object convert(String srcStr, Class dstClass, AtomicInteger curIndex) throws Exception {
 
     Object retVal = dstClass.forName(dstClass.getName()).newInstance();
+    Object attachment = null;
     StringBuilder chars = new StringBuilder();
 
     String key = "", val;
@@ -33,68 +34,67 @@ public class StrNPOJO {
 
     PropertyDescriptor propertyDescriptor = null;
 
-    while (curIndex.intValue() < srcStr.length()) {
+    scanner: while (curIndex.intValue() < srcStr.length()) {
       Character curChar = srcStr.charAt(curIndex.intValue());
 
-      if (curChar == '{') {
-        if (key.length() != 0){
-          Object subObj = convert(srcStr, typeHashMap.get(key), curIndex);
-          propertyDescriptor.getWriteMethod().invoke(retVal, subObj);
-        } else{
-          curlyOpen++;
-        }
-
-      } else if(curChar == '}'){
-        curlyOpen--;
-        break;
-      } else if ((curChar == ' ') || (curChar == '\n')) {
-        // ignore whitespaces everywhere except inside the key and value name.
-        if (!ignoreSpaces) {
-          chars.append(curChar);
-        }
-      } else if (curChar == '"') {
-        ignoreSpaces = !ignoreSpaces;
-      } else if (curChar == ',') {
-
-        val = new String(chars);
-
-        if (!readingArray && chars.toString().length() != 0) {
-          propertyDescriptor
-              .getWriteMethod()
-              .invoke(retVal, this.objectGetters.getObject(val, typeHashMap.get(key)));
+      switch (curChar){
+        case '{':
+          if (key.length() != 0){
+            attachment = convert(srcStr, typeHashMap.get(key), curIndex);
+          } else{
+            curlyOpen++;
+          }
+          break;
+        case '}':
+          curlyOpen--;
+          break scanner;
+        case ' ':
+          if (!ignoreSpaces) chars.append(curChar);
+          break;
+        case '\n':
+          if (!ignoreSpaces) chars.append(curChar);
+          break;
+        case '"':
+          ignoreSpaces = !ignoreSpaces;
+          break;
+        case ',':
+          val = new String(chars);
+          if (!readingArray && chars.toString().length() != 0) {
+            attachment = this.objectGetters.getObject(val, typeHashMap.get(key));
+            chars = new StringBuilder();
+          } else {
+            arrayList.add(val);
+            chars = new StringBuilder();
+          }
+          break;
+        case ':':
+          key = new String(chars);
           chars = new StringBuilder();
-        } else {
+          propertyDescriptor = new PropertyDescriptor(key, dstClass);
+          break;
+        case '[':
+          squareOpen++;
+          arrayList = new ArrayList<>();
+          readingArray = true;
+          break ;
+        case ']':
+          squareOpen--;
+          readingArray = false;
+          val = new String(chars);
+          chars = new StringBuilder();
+
           arrayList.add(val);
-          chars = new StringBuilder();
-        }
-
-      } else if (curChar == ':') {
-
-        key = new String(chars);
-        chars = new StringBuilder();
-        propertyDescriptor = new PropertyDescriptor(key, dstClass);
-
-      } else if (curChar == '[') {
-        squareOpen++;
-        arrayList = new ArrayList<>();
-        readingArray = true;
-
-      } else if (curChar == ']') {
-        squareOpen--;
-        readingArray = false;
-
-        val = new String(chars);
-        chars = new StringBuilder();
-        arrayList.add(val);
-
-        Class baseType = typeHashMap.get(key).getComponentType();
-        Object attachment = this.objectGetters.getObject(arrayList, baseType);
-        propertyDescriptor.getWriteMethod().invoke(retVal, attachment);
-
-      } else {
-        chars.append(curChar);
+          attachment = this.objectGetters.getObject(arrayList, typeHashMap.get(key).getComponentType());
+          break;
+        default:
+          chars.append(curChar);
+          break;
       }
 
+      if (attachment != null){
+        propertyDescriptor.getWriteMethod().invoke(retVal, attachment);
+        attachment = null;
+      }
       curIndex.set(curIndex.intValue() + 1);
     }
 
